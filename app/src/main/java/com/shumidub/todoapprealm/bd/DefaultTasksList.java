752fd5d076a348422ebbc78d410437698a5c0417
ShumidubCoder <shumidubcoder@gmail.com>
2017-12-25 06:00:42 +0300

    add category activity and more

---
+++ app/src/main/java/com/shumidub/todoapprealm/bd/DefaultTasksList.java
@@ -0,0 +1,12 @@
+package com.shumidub.todoapprealm.bd;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by Артем on 25.12.2017.
+ */
+
+public class DefaultTasksList extends RealmObject {
+
+    //if null - all
+}
