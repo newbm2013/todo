26a54ca8cd36b9f9b345e525c182aa798afaa0f5
A.Shumidub <Qaor25520>
2018-03-02 16:40:56 +0300

    +++

--- app/src/main/java/com/shumidub/todoapprealm/ui/fragment/task_section/folder_panel_sliding_fragment/adapter/FolderOfTaskRecyclerViewAdapter.java
+++ app/src/main/java/com/shumidub/todoapprealm/ui/fragment/task_section/folder_panel_sliding_fragment/adapter/FolderOfTaskRecyclerViewAdapter.java
@@ -129,8 +129,6 @@ public class FolderOfTaskRecyclerViewAdapter
             //todo default color ?
 //            ((ItemViewHolder) holder).tvFolderTaskCounts.setTextColor(Color.BLUE);
         }
-
-
     }
 
 
